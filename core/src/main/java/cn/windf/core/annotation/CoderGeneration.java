ebc672bc66ed6d6fa1430b8504ae469c69e6abb4
windf <q3400p@sina.cn>
2020-06-14 14:31:29 +0800

    重构：注解，标记是代码生成的，有点强制的

---
+++ core/src/main/java/cn/windf/core/annotation/CoderGeneration.java
@@ -0,0 +1,12 @@
+package cn.windf.core.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 代码生成的注释，如果有此注释，则证明是代码生成的
+ */
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.FIELD, ElementType.METHOD})
+public @interface CoderGeneration {
+}
