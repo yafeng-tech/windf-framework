1cb6cfb82b9a3b91681aa1e40a2d4037b928ad06
windf <q3400p@sina.cn>
2020-08-15 19:29:26 +0800

    功能：添加mybatis基础接口,继承存储接口

--- plugin-repository-mybatis/src/main/java/cn/windf/plugin/repository/mybatis/BaseRepository.java
+++ plugin-repository-mybatis/src/main/java/cn/windf/plugin/repository/mybatis/BaseRepository.java
@@ -1,11 +1,34 @@
 package cn.windf.plugin.repository.mybatis;
 
+import cn.windf.core.entity.BaseEntity;
+import cn.windf.core.entity.Page;
+import cn.windf.core.entity.SearchData;
+import cn.windf.core.repository.ManageRepository;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
 
 /**
  * 基础接口，所有mangger都要继承她
  * @param <T>
  */
-public interface BaseRepository<T> extends BaseMapper<T> {
+public interface BaseRepository<T extends BaseEntity> extends BaseMapper<T>, ManageRepository<T> {
+    default void create(T entity) {
+        insert(entity);
+    }
+
+    default void update(T entity) {
+        updateById(entity);
+    }
+
+    default void delete(List<String> ids) {
+        deleteBatchIds(ids);
+    }
+
+    default T detail(String id) {
+        return selectById(id);
+    }
+
+    Page<T> search(SearchData searchData);
 
 }
